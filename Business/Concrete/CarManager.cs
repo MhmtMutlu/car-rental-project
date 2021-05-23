f8a0f400076cd5128512b4382382c9d4b1425b39
Mehmet Mutlu <mhmetmtlu@users.github.com>
2021-05-23 15:34:30 +0300

    Brand, Color entities and database connections has been created.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,5 +18,15 @@ namespace Business.Concrete
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetAllByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetByDailyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(p => p.DailyPrice <= max && p.DailyPrice >= min);
+        }
     }
 }
