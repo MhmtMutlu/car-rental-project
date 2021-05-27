d0ed346f89aa5689df83f8da59f6a24e4d2fbd58
Mehmet Mutlu <mhmetmtlu@users.github.com>
2021-05-27 21:36:14 +0300

    Core Layer has been created

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,62 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            // IDispossable pattern implementation of C#
+            using (TContext context = new TContext())
+            {
+                var addedEntitiy = context.Entry(entity); // entity olarak gelen ögeyi yakaladık
+                addedEntitiy.State = EntityState.Added; // ef komutları ekledik
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntitiy = context.Entry(entity);
+                deletedEntitiy.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntitiy = context.Entry(entity);
+                updatedEntitiy.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/CarRentalContext.cs
+++ DataAccess/Concrete/EntityFramework/CarRentalContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    class CarRentalContext:DbContext
+    public class CarRentalContext:DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
